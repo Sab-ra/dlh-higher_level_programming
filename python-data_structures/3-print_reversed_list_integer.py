108edc3acd34729658c4e4153d2e95f111f6c077
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-04-29 16:58:23 +0200

    Remove () around [::1]

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    reversed_list = my_list([::-1])
+    reversed_list = my_list[::-1]
     for i in reversed_list:
         print("{:d}".format(i))
