550c854dedce4631761d659311153c331730c7f9
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-04-29 18:10:23 +0200

    Task 5 with intermediary array

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    your_ctring = ""
-    for c in my_string:
-        if c != "c" or c != "C":
-            your_string = my_string + c
-    return your_string
+    char_list = list(my_string)
+    new_string = ''
+    for i in char_list:
+        if i == 'c':
+            continue
+        elif i == 'C':
+            continue
+        else:
+            new_string = new_string + i
+        return new_string
