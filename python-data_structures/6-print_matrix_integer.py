0184f2339cef47222508b0cc1560098fde614815
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-04-29 22:29:14 +0200

    Add space between raw values

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for idx in range(len(row)):
             if idx < len(row) - 1:
-                print("{:d}".format(row[idx]), end='')
+                print("{:d }".format(row[idx]), end='')
             else:
                 print("{:d}".format(row[idx]))
