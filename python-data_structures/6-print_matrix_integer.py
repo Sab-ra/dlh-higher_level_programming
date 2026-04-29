e672ab5312e46ad7a57c39806a185da5a7c03c14
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-04-29 22:38:22 +0200

    Another try

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for idx in range(len(row)):
             if len(row) == 0:
-                print()
+                print(" ")
             elif idx < len(row) - 1:
                 print("{:d} ".format(row[idx]), end='')
             else:
