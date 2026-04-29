ce662ee0cf3ec6272a66a96f3e224f490e9c065f
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-04-29 17:20:04 +0200

    Add for to populate new_list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    new_list = []
+    for i in my_list:
+        new_list.append(i)
     if idx < 0:
-        return my_list
+        return new_list
     elif idx >= len(my_list):
-        return my_list
+        return new_list
     else:
-        my_list[idx] = element
-        return my_list
+        new_list[idx] = element
+        return new_list
