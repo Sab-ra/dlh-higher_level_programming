be70046f6d99a5b4a84ffe00ba8e5f409894de43
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-04-30 16:19:38 +0200

    Task 11

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0)
+    if len(my_list) == 0:
+        return None
+    elif idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        i = 0
+        result = []
+        while i < len(my_list):
+            if i == idx:
+                i += 1
+                continue
+            else:
+                result.append(my_list[i])
+            	i += 1
+        return result
