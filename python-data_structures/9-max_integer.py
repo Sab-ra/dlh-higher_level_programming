e50048681a5fa0287a006cb25c9c52029864585f
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-04-30 15:03:16 +0200

    Task 9. Normal hit

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     else:
-        a = min(my_list)
+        a = my_list[0]
         while i < len(my_list):
             if a >= my_list[i]:
                 i += 1
