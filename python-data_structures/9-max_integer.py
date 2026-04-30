7272aceb9606f8963e668a084aabc22dcc89b0b6
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-04-30 14:51:20 +0200

    Polish hack

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     i = 0
-    a = min(my_list)
     if len(my_list) == 0:
         return None
     else:
+        a = min(my_list)
         while i < len(my_list):
             if a >= my_list[i]:
                 i += 1
