0944d0bff7133e4e7384abd416a95575dc188f11
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-04-30 09:51:49 +0200

    Get rid of max()

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    i = 0
+    a = 0
     if len(my_list) == 0:
         return None
     else:
-        return max(my_list)
+        while i < len(my_list):
+            if a >= my_list[i]:
+            i += 1
+            continue
+        else:
+            a = my_list[i]
+            i += 1
+        return a
