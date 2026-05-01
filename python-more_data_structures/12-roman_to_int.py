6d59a098c3235e3d0912798efbb6abbd6657a48c
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-01 18:11:28 +0200

    Not string > str

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) is not string:
+    if type(roman_string) is not str:
         return 0
     elif type(roman_string) is None:
         return 0
