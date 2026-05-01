11e0f4bfc18ed962c1c20488f6e325e0d8a0e7fa
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-01 14:17:43 +0200

    Use while loop

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in my_list:
+    i = 0
+    while i < len(my_list):
         if my_list[i] == search:
             my_list[i] = replace
+        return my_list
