0846b4cb30ba8f166162cc5638d766075fd80d2d
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-01 17:13:44 +0200

    Return key by result, hopefully

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    result = ''
     if a_dictionary == {}:
         return None
+        break
+    elif a_dictionary == None:
+        return None
+        break
     else:
         ch_list = list(a_dictionary)
         max_val = a_dictionary[ch_list[0]]
         for key in ch_list:
-             if max_val < a_dictionary[key]:
-                 max_val = a_dictionary[key]
-    return max_val
-             
+            if max_val < a_dictionary[key]:
+                max_val = a_dictionary[key]
+    for key in ch_list:
+        if a_dictionary[key] = max_value:
+            result = key
+    return result
