1fb897c13e94bb327b77d132a31e680691428be5
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-01 18:06:34 +0200

    Correct exceptions

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_sting) is not string or type(roman_string) is None:
+    if type(roman_sting) is not string:
+        return 0
+    elif type(roman_string) is None:
         return 0
     else:
         pass
