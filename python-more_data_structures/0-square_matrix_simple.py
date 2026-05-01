8b5476950f1684b1b8b7c1938cde26a8e7bf30d8
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-01 11:25:05 +0200

    Trying nesting

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,5 @@ def square_matrix_simple(matrix=[]):
     resultrix = []
     for row in matrix:
         for i in row:
-            resultrix.append(i**2)
+            resultrix[raw].append(i**2)
     return resultrix
