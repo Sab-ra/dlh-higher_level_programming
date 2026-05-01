bad611732ce83644e23bf2ae9b8a94e21f4810c1
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-01 16:20:22 +0200

    Task 9.1

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -3,6 +3,6 @@ def multiply_by_2(a_dictionary):
     result = {}
     i = 0
     while i < len(a_dictionary):
-        result[(list(a_dictionary)[i])] = a_dictionary[list(a_dictionary)[i] * 2]
+        result[(list(a_dictionary)[i])] = [a_dictionary[list(a_dictionary)[i]] * 2]
         i += 1
     return result
