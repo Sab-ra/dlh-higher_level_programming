9152dd42d7509b71a1aaa7d02f0a1bcf59d03085
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-01 16:35:20 +0200

    Shorten the longest line

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    result = {}
+    rslt = {}
     i = 0
     while i < len(a_dictionary):
-        result[(list(a_dictionary)[i])] = a_dictionary[list(a_dictionary)[i]] * 2
+        rslt[(list(a_dictionary)[i])] = a_dictionary[list(a_dictionary)[i]] * 2
         i += 1
-    return result
+    return rslt
