1cb90163019c292e0d328485d0ab0d592fbd7bb0
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-01 19:14:22 +0200

    Task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,35 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    roman_weights = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     if type(roman_string) is not str:
         return 0
     elif type(roman_string) is None:
         return 0
     else:
-        pass
+        roman_string = roman_string.upper()
+        arab_list = []
+        for c in roman_string:
+            arab_list.append(roman_weights[c])
+    num_list = []
+    for i in arab_list:
+        num_list.append(arab_list[0])
+        if i > 0:
+             if arab_list[i] > arab_list[i - 1]:
+                 if arab_list[i] == 1000:
+                     del num_list[-1]
+                     num_list[-1] = 900
+                 if arab_list[i] == 500:
+                     del num_list[-1]
+                     num_list[-1] = 400
+                 if arab_list[i] == 100:
+                     del num_list[-1]
+                     num_list[-1] = 90
+                 if arab_list[i] == 50:
+                     del num_list[-1]
+                     num_list[-1] = 40
+                 if arab_list[i] == 10:
+                     del num_list[-1]
+                     numb_list[-1] = 9
+                 if arab_list[i] == 5:
+                     del num_list[-1]
+                     numb_list[-1] = 4
