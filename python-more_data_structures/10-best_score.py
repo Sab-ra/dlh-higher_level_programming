d130618c4280e40d8a9577d60ed9da7cda17fe51
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-01 16:54:03 +0200

    Task 10. Max mark among students

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,4 +3,10 @@ def best_score(a_dictionary):
     if a_dictionary == {}:
         return None
     else:
-        pass
+        ch_list = list(a_dictionary)
+        max_val = a_dictionary[ch_list[0]]
+        for key in ch_list:
+             if max_val < a_dictionary[key]:
+                 max_val = a_dictionary[key]
+    return max_val
+             
