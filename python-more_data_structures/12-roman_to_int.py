0b77d795de40690fc4159a8305edbdf015ed5cf8
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-01 18:04:16 +0200

    Prepare files and exception check 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_sting) is not string or type(roman_string) is None:
+        return 0
+    else:
+        pass
--- python-more_data_structures/README.md
+++ python-more_data_structures/README.md
@@ -7,3 +7,4 @@
 6-print_sorted_dictionary.py
 7-update_dictionary.py
 8-simple_delete.py
+12-roman_to_int.py
