96b862cbb0fa4d84b31807388b53c033949b6c04
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-02 16:04:11 +0200

    Zip roman_weights

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_weights = {'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
+    roman = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
+    weights = [1000, 500, 100, 50, 10, 5, 1]
+    roman_weights = dict(zip(roman, weights))
     if roman_string is None or not isinstance(roman_string, str):
         return 0
     else:
