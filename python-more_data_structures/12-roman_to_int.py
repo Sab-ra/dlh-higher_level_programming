c7b73fb6bdd2b8fbef7d1d70eb97358fa40f6594
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-02 15:20:04 +0200

    Figuring out why don't accept initial checks

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,14 +3,15 @@ def roman_to_int(roman_string):
     roman_weights = { 'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1 }
     if roman_string is None or is not isinstanceof(roman_string, str):
         return 0
-    nine_force = []
-    for c in roman_string:
-        weight = riman_weights[c]
-        if len(nine_force) > 0:
-            if weight > nine_force[-1]:
-                nine_force[-1] = weight - nine_force[-1]
+    else:
+        nine_force = []
+        for c in roman_string:
+            weight = riman_weights[c]
+            if len(nine_force) > 0:
+                if weight > nine_force[-1]:
+                    nine_force[-1] = weight - nine_force[-1]
+                else:
+                    nine_force.append(weight)
             else:
                 nine_force.append(weight)
-        else:
-            nine_force.append(weight)
     return sum(nine_force)
