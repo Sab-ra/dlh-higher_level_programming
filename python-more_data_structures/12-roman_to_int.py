263b8f1739591e9d00c26c282a8eada602cbe398
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-02 15:53:48 +0200

    Fuck

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman_weights = { 'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1 }
-    if roman_string is None:
-        return 0
-    elif roman_string not isinstance(roman_string, str):
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
     else:
         nine_force = []
