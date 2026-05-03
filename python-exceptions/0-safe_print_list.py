77611314645d6769c1b2236a64fc74b8efde3f90
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-03 14:08:06 +0200

    Task 0.1

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,6 +5,6 @@ def safe_print_list(my_list=[], x=0):
         for i in range(0, x):
             print("{} ".format(my_list[i]), end='')
             counter += 1
-    except:
+    except IndexError:
         pass
     return counter
