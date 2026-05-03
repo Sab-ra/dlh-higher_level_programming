f6691ac47c8eced256c87583a993b6abe565847b
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-03 21:00:11 +0200

    __import__sys

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import sys
 
 def safe_print_integer_err(value):
     try:
-        if isinstance(value, bool):
-            raise TypeError("an integer is required")
         print("{:d}".format(value))
         return True
     except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr)
+        print(
+            "Exception: {}".format(e),
+            file=__import__('sys').stderr
+        )
         return False
