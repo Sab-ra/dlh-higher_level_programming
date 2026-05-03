4f9ec91e14997b9b914a615dc264a0f9fbd86993
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-03 14:18:55 +0200

    Broad except

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(0, x):
             print("{} ".format(my_list[i]), end='')
             counter += 1
-    except IndexError:
+    except:
         pass
     print()
     return counter
