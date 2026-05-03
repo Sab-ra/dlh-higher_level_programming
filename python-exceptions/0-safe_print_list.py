5e71df7c45f633a78f03f4b79a97074947d3e577
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-03 14:01:59 +0200

    Fix indentation

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@ def safe_print_list(my_list=[], x=0):
     counter = 0
     try:
         for i in range(0, x):
-        print("{} ".format(my_list[i]), end='')
-        counter += 1
+            print("{} ".format(my_list[i]), end='')
+            counter += 1
     except:
         pass
     return counter
