cf4db62710f6e257125ffd8d6efa38ea58fe0b6f
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-03 20:56:11 +0200

    what?

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
+
 def safe_print_integer_err(value):
     try:
         if isinstance(value, bool):
-            value = "Bool"
+            raise TypeError("an integer is required")
         print("{:d}".format(value))
         return True
     except Exception as e:
