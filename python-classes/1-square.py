82333aab48610512bc2ed56dc35d28002e736ab9
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-05 17:49:41 +0200

    Create squares with defined sises

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module with class that defines square by size"""
+
+
+class Square:
+    """Initialises object square with private size."""
+    def __init__(self, size):
+        self.__size = size
--- python-classes/README.md
+++ python-classes/README.md
@@ -1,1 +1,2 @@
 0-square.py
+1-square.py
