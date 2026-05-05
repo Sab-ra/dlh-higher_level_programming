d2476020af3bf6b4324da245ee7bee3017afd621
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-05 19:35:50 +0200

    Edited task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,20 +5,26 @@
 class Square:
     """Initialises object square after size validation"""
     def __init__(self, size=0):
-        self.__size = set_size
+        """Initialize a square with size validation.
+        Args:
+            size: The size of the square (must be an integer >= 0).
+        """
+        self.set_size(__size)
 
-    """Public property - area"""
     def area(self):
+        """Return current square area"""
         return self.__size ** 2
 
     """Getters and Setters"""
+
     def get_size(self):
         """Check current sise of the square object"""
         return self.__size
 
     def set_size(self, size):
+        """Single sourse of truth to set private property size of the square"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        return size
+        self._size = size
