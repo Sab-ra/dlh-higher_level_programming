c7795507e4a04f0b52927fcedd9b48953c4c7e24
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-05 18:28:51 +0200

    Let user access area of square object

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module with class that defines square with publicly available area"""
+
+
+class Square:
+    """Initialises object square after size validation"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    """Public property - area"""
+    def area(self):
+        return size ** 2
