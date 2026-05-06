6954de1e4a33a01e3133e9e85fc841a3cba72f5f
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-06 16:36:55 +0200

    Consider zero side perimeter exception

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -22,7 +22,10 @@ class Rectangle:
 
     def perimeter(self):
         """Returns rectangle perimeter"""
-        return 2 * (self.__width + self.__height)
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
 
     """Getters & Setters"""
 
