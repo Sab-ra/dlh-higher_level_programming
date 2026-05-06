08a499c2b6904809806a878d3aa226195256f7be
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-07 00:42:15 +0200

    Blank spaces

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -109,7 +109,7 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
 
         return rect_2 if rect_2.area() > rect_1.area() else rect_1
-        
+
     # 6. Helpers
 
     def measures_ok(self):
