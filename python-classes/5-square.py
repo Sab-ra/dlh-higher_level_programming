0947e66fee6b6870fb0d6ed8240ca14ca3c1e123
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-06 09:26:41 +0200

    Draw the square

---
+++ python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Module with class that defines square with publicly available area and adjustable size"""
+
+
+class Square:
+    """Initialises object square after size validation"""
+    def __init__(self, size=0):
+        """Initialize a square through the setter."""
+        self.size = size
+
+    def area(self):
+        """Return current square area"""
+        return self.__size ** 2
+    
+    def my_print(self):
+        """Prints the square"""
+        if self.__size > 0:
+            x = self.__size
+            for i in range(x):
+                print("#" * x)
+        else:
+            print()
+
+
+    """Getters and Setters"""
+
+    @property
+    def size(self):
+        """Check current sise of the square object"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Single sourse of truth to set private property size of the square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
