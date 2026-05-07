20ab1c50b68eb383e3c5c22a296f616f47570b11
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-07 22:41:23 +0200

    Stringify before writing

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,4 +5,5 @@
 def write_file(filename="", text=""):
     """Filename may be relative or absolute"""
     with open(filename, mode="w", encoding="utf-8") as blank_page:
-        blank_page.write(text)
+        s = str(text)
+        blank_page.write(s)
