06c40a83c7f543f8182765f07f1016d75b6dde07
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-07 22:46:52 +0200

    Return write(), not just call

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,4 +6,4 @@ def write_file(filename="", text=""):
     """Filename may be relative or absolute"""
     with open(filename, mode="w", encoding="utf-8") as blank_page:
         s = str(text)
-        blank_page.write(s)
+        return blank_page.write(s)
