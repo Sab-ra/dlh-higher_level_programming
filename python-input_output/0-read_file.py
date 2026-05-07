e1be1c95ef9dbb6352e3f46804ea797d6f18602b
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-07 19:42:30 +0200

    Remove empty line in the end of print

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """If file bit enough--your comp will dye"""
     with open(filename, mode="r", encoding="utf-8") as file_to_read:
-        print(file_to_read.read())
+        print(file_to_read.read(), end="")
