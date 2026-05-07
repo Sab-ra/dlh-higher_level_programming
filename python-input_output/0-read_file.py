6b3ccabe8e198487511dd83c0775a8f487beb7d6
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-07 19:35:56 +0200

    Add module commentar

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module that contains a function to read and print file. Bery-bery dandjurious module"""
+
+
 def read_file(filename=""):
     with open(filename, mode="r", encoding="utf-8") as file_to_read:
         print(file_to_read.read())
