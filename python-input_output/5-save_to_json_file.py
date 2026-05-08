d11f64ee96ba27a1d8de15d755aa4045c6826216
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-08 12:40:22 +0200

    Put file object behind the filename

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,6 +6,6 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Will brake on pics"""
-    with open(filename, mode="w", encoding="utf-8"):
-        return filename.write(json.dumps(my_obj))
+    """Under filename you need a file object"""
+    with open(filename, mode="w", encoding="utf-8") as file_object:
+        return json.dump(my_obj, file_object)
