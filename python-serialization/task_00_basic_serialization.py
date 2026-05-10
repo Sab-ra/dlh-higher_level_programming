f9a7598e8705497e36a7662e802db361b5357bd0
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-10 16:39:56 +0200

    Serialize and Deserialize

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module to serialize and deserialize"""
+
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """Data to file"""
+    with open(filename, "w") as f:
+        json.dump(data, f)
+
+def load_and_deserialize(filename):
+    """Data from file"""
+    with open(filename) as f:
+        try:
+            json.load(f)
+        except (FileNotFoundError, json.JSONDecodeError):
+            return {}
