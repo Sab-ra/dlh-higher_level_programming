af65c5c1c62fa842bacb5ddc1073c182c3e0c616
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-10 18:04:55 +0200

    Pickle object of the class

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Serialize with pickle"""
+
+
+import pickle
+
+
+class CustomObject:
+    """Custom class that supports pickle ser-tion."""
+
+    def __init__(self, name, age, is_student):
+        """Initialize object"""
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Prints info of object"""
+        for key, value in self.__dict__.items():
+            print("{}: {}".format(key.replace("_", "").title(), value))
+
+    def serialize(self, filename):
+        """Self to pickle file"""
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+        except (OSError, pickle.PicklingError):
+            return None
+
+    @classmethod
+    def deserizlize(cls, filename):
+        """Become unknown object"""
+        try:
+            with open(filename, "rb") as f:
+                obj = pickle.load(f)
+            return obj if isinstance(obj, cls) else None
+        except (FileNotFoundError, OSError, EOFError, pickle.UnpicklingError):
+            return None
