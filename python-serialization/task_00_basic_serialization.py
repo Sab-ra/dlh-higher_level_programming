24a4f8829ffacd99ffff34688cafb621423054b6
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-05-10 16:52:25 +0200

    Return deserialized

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -9,12 +9,11 @@ def serialize_and_save_to_file(data, filename):
     """Data to file"""
     with open(filename, "w") as f:
         json.dump(data, f)
-    print("Data serialized and saved to {}.".format(filename))
 
 def load_and_deserialize(filename):
     """Data from file"""
     with open(filename) as f:
         try:
-            json.load(f)
+            return json.load(f)
         except (FileNotFoundError, json.JSONDecodeError):
             return {}
