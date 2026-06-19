eb08525af2dbbafd31d8dcfa1bf7144ef0bc12fe
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-06-19 14:55:16 +0200

    Fix position validation logic in 6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -50,7 +50,7 @@ class Square:
         """Sets position on birth of an instance, or during it's life"""
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
-        if not all(isinstance(num, int) and num >= 0 for num in value:
+        if not all(isinstance(num, int) or num >= 0) for num in value:
             raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
             
