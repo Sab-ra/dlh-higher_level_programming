4eefa80d8f54f8dd98de131fd8fa314b0ba5dcaf
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2026-06-19 14:44:42 +0200

    Correct position validation logic in 6-square.py
    Fix position validation to check both elements are positive integers.

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -50,7 +50,9 @@ class Square:
         """Sets position on birth of an instance, or during it's life"""
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
-        elif value[0] < 0 or value[1]:
+        elif not all(isinstance(value, int):
+            raise TypeError('position must be a tuple of 2 positive integers')
+        elif value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
         else:
             self.__position = value
